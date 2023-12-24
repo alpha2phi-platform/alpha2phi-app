6b4a7be011cf0cabbae4640c15febe9c5486a525
alpha2phi <alpha2phi@gmail.com>
2023-12-24 23:37:32 +0800

    refactor: template project

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import Theme from "./providers/Theme";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
