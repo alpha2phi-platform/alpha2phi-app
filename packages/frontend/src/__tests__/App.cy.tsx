8a0a0d12df98811bdf05f301f1b770a07e111a5e
alpha2phi <alpha2phi@gmail.com>
2023-12-27 21:11:34 +0800

    feat: cypress

---
+++ packages/frontend/src/__tests__/App.cy.tsx
@@ -0,0 +1,8 @@
+import { mount } from '@cypress/react18'
+import App from '../App'
+
+describe('<App>', () => {
+  it('mounts', () => {
+    mount(<App />)
+  })
+})
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import istanbul from "vite-plugin-istanbul";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    istanbul({
+      cypress: true,
+      requireEnv: false,
+    })
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
+  },
+  server: {
+    host: true,
+    port: 3000,
   },
 })
