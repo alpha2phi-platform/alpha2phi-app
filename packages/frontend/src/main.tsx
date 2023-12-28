0c0a342a77b4f75b3f5f2b0548a93f0304c33ee9
alpha2phi <alpha2phi@gmail.com>
2023-12-28 21:52:36 +0800

    chore: remove un-used imports and update dependencies

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
-import Theme from "./providers/Theme";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import Theme from "./providers/Theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={Theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={Theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>
 );
