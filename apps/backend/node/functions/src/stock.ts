79d849ada631b1e1352749778610c47748a189a5
mengwangk <mengwangk@gmail.com>
2024-01-04 20:51:02 +0800

    refactor: folder layout

--- apps/backend/node/functions/src/stock.ts
+++ apps/backend/node/functions/src/stock.ts
@@ -2,7 +2,7 @@ import { Table } from "sst/node/table";
 import handler from "@alpha2phi/core/handler";
 import dynamoDb from "@alpha2phi/core/dynamodb";
 
-export const list = handler(async (event) => {
+export const list = handler(async (_event) => {
   const params = {
     TableName: Table.Stock.tableName,
   };
