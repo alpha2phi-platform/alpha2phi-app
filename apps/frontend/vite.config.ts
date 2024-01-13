329ec4dd06fb40b9b8c4a5aad8a3c4276741acf8
mengwangk <mengwangk@gmail.com>
2024-01-13 19:31:50 +0800

    fix: resolve import @

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import istanbul from "vite-plugin-istanbul";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,5 +21,8 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
   },
 });
