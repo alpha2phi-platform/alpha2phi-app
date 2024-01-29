f42c87e8e8cd363be1076eeb0a3a62aaafc5cc45
mengwangk <mengwangk@gmail.com>
2024-01-29 18:43:27 +0800

    docs: update

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -5,6 +5,28 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import Theme from "./providers/Theme";
+import { Amplify } from "aws-amplify";
+import config from "./config";
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        primary_endpoint: config.apiGateway.PRIMARY_API_URL,
+        secondary_endpoint: config.apiGateway.SECONDARY_API_URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
